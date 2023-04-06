5b87badbad0663c2a017a1a9ef68d0a756a9eba2
jhin <com.jinnn99@gmail.com>
2023-04-06 20:06:35 +0900

    15. 로그인 로직 구현

---
+++ todo-react-app/src/AppRouter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './index.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Box, Typography } from '@material-ui/core';
+import App from './App';
+import Login from './Login';
+
+function Copyright(){
+    return(
+        <Typography variant='body2' color='textSecondary' align='center'>
+            {"Copyright ⓒ"}
+            fsoftwareenengineer, {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
+
+function AppRouter(){
+    return(
+        <BrowserRouter>
+            <div>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/" element={<App />} />
+                </Routes>
+            </div>
+            <div>
+                <Box mt={5}>
+                    <Copyright />
+                </Box>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default AppRouter;
