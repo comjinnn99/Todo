74735602ed0ab91d4e30cbddba48a52e257d1967
jhin <com.jinnn99@gmail.com>
2023-04-06 09:07:30 +0900

    props, state 사용하기

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Hello from './Hello';
+import Todo from './Todo';
 import './App.css';
 
 function App() {
-    return (
-      <div className="App">
-        <Hello />
-      </div>
-    );
+  const item = { id: 0, title: "Hello World 1", done: true };
+  return (
+    <div className="App">
+      <Todo item={item} />
+    </div>
+  );
 }
 
 export default App;
